4ad68d537178c416d0e234de3c4e3cd99fdfc9e2
Eitan Pano <eitanpano95@gmail.com>
2021-10-04 11:42:51 +0300

    DOM and model connection

--- js/app.js
+++ js/app.js
@@ -1,16 +1,34 @@
 'use strict';
 // DOM JS FILE
 
-renderBoard(gBoard);
+
+
+
+function init() {
+    renderBoard(gBoard);
+    
+}
+
 
 function renderBoard(board){
     var strHTML = '';
     for (var i = 0; i < board.length; i++) {
         strHTML += `<tr>`
         for (var j = 0; j < board[0].length; j++) {
-            strHTML += `<td id="${i,j}" onclick="cellClicked(this)"></td>`
+            strHTML += `<td data-cell="${i}-${j}" onclick="cellClicked(this)"></td>`
         }
         strHTML += `</tr>`
     }
     document.querySelector('.game-board table').innerHTML = strHTML;
+}
+
+function cellClicked(el) {
+    console.log(el);
+    // reveal
+
+}
+
+function updateScore(diff) {
+    gGame.score += diff;
+    document.querySelector('h2 span').innerText = gGame.score;
 }
--- js/minesweeper.js
+++ js/minesweeper.js
@@ -1,17 +1,42 @@
 'use strict';
 // MODEL JS FILE
 
+const MINE = '💣';
+const FLAG = '🚩'
+
 var gBoard = createBoard(8);
 
 function createBoard(size, bombNum) {
     var board = [];
+
     for (var i = 0; i < size; i++) {
         var row = [];
         for (var j = 0; j < size; j++) {
-            var col = { i, j };
+            var col = {
+                i,
+                j,
+                status: 'hidden'
+            };
             row.push(col);
         }
         board.push(row);
     }
     return board;
 }
+
+function genItem(itemName = CHERRY) {
+    var validCells = [];
+    for (var i = 0; i < gBoard.length; i++) {
+        for (var j = 0; j < gBoard[0].length; j++) {
+            var currCell = gBoard[i][j];
+            if (currCell === EMPTY) {
+                validCells.push({ i, j });
+            }
+        }
+    }
+    if (!validCells.length) return;
+    var randomCell = validCells[getRandomInt(0, validCells.length - 1)];
+    gBoard[randomCell.i][randomCell.j] = CHERRY;
+    renderCell(randomCell, itemName);
+}
+
